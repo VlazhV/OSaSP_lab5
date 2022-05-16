80af2e16e3e6e7fb7cda4b13a7b714584f70689a
vlazhv <danilabondarev1234@gmail.com>
2022-05-17 00:34:03 +0300

    ready

--- ind8.c
+++ ind8.c
@@ -12,6 +12,8 @@ typedef struct
 	
 	char *buf_full;
 	size_t buf_full_size;
+	
+	FILE *f_out;
 }thread_info;
 
 
@@ -19,6 +21,8 @@ typedef struct
 void *thread_check(void *data)
 {
 	thread_info *info = (thread_info *)data;
+	
+	FILE *fout = info->f_out;
 	
 	for (size_t i = 0; i < info->buf_sep_size; ++i)
 	{
@@ -33,7 +37,7 @@ void *thread_check(void *data)
 				while (info->buf_sep[++i] == info->buf_full[++j])
 					++len;
 					
-				printf("Macth: file1 = %ld[%u]  ---- file2 = %ld ---- len = %ld \n", i_save, curr_char, j_save, len);
+				fprintf(fout, "Macth [%u - %c]: file1 = %ld  ---- file2 = %ld ---- len = %ld \n", curr_char, curr_char, i_save, j_save, len);
 				
 				i = i_save;
 				j = j_save;
@@ -41,7 +45,6 @@ void *thread_check(void *data)
 		}
 		
 	}
-	
 	pthread_exit(0);
 }
 
@@ -83,6 +86,12 @@ size_t readfile(char *filename, char **buf)
 	for (size_t i = 0; i < filesize; ++i)
 		(*buf)[i] = getc(f);
 		
+	res = fclose(f);
+	if (res != 0)
+	{
+		perror("error rf5 : can't close file");
+		return -1;
+	}
 	
 	return filesize;
 }
@@ -136,6 +145,13 @@ int main(int argc, char *argv[])
 	int nThreads = convertN(argv[3]);
 	if (nThreads == -1)
 		return -1;
+		
+	FILE *f_out = fopen(argv[4], "w");
+	if (f_out == NULL)
+	{
+		perror ("error : can't open fileout");
+		return -1;
+	}
 	
 	char *buf1, *buf2;
 	
@@ -182,11 +198,16 @@ int main(int argc, char *argv[])
 		}
 	}
 			
-//puts(buf1);
+	int res;	
 
+	pthread_t pt_arr[nThreads];
+	
 	for (int i = 0; i < nThreads; ++i)
 	{
 		pthread_t pt;
+		
+		
+		
 		
 		thread_info *info = (thread_info*)calloc(1, sizeof(thread_info));
 		if (info == NULL)
@@ -199,17 +220,48 @@ int main(int argc, char *argv[])
 		info->buf_sep_size = blocksize;
 		info->buf_full = fullfile;
 		info->buf_full_size = fullfilesize;
+		info->f_out = f_out;
 		
-		pthread_create(&pt, NULL, &thread_check, info);
+		res = pthread_create(&pt, NULL, &thread_check, info);
+		if (res != 0)
+		{
+			fprintf(stderr, "error : thread %d can't create\n", i);
+			perror(" ");
+		}
+		else
+		{
+			printf("%d created\n", i);
+			pt_arr[i] = pt;
+		}
 	}
 	
 	
+
+	void *retval;
+	for (int i = 0; i < nThreads; ++i)
+	{
+		res = pthread_join(pt_arr[i], &retval);
+		
+		if (res != 0)
+		{
+			fprintf(stderr, "error: can't join thread %d", i);
+			perror(" ");
+		}
+		if ( retval != 	NULL)
+			fprintf(stderr, "thread %d terminated UNsuccessfully\n", i);
+			
+	}
 	
-	printf("blocksize = %ld", blocksize);
-	printf("\n[%ld]\n", buf1size);
 	
-//puts(buf2);
-	printf("\n[%ld]\n", buf2size);
+
+	free(buf1);
+	free(buf2);
 	
+	res = fclose(f_out);
+	if (res != 0)
+	{
+		perror("error : can't close fileout");
+		return -1;
+	}
 	return 0;
 }
